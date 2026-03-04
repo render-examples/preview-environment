4e63ce29d7d72a4358ecbbaee132b35569254f5a
Rose Koron <rose@render.com>
2026-03-04 15:39:45 -0800

    Add initial skeleton

--- backend/seed.js
+++ backend/seed.js
@@ -1,7 +1,7 @@
 let Todo = require('./todoItem')
 
 const seedValues = [
-    "Be excited about Preview Environments!",
+    "Be excited about Preview Environments! Wow!",
     "Align the picture frames of our ancestors",
     "Prepare table in a way that reflects warmth, care, and slight discomfort - a distinctive quality of a family home",
     "Make toast with a balanced mix of delight and sorrow",
